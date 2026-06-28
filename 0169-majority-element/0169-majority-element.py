026c6a815e6bdfc86fda1eaab380693d8f60d432
Anushka Thakur <justanushka06@gmail.com>
2026-06-28 16:40:57 +0530

    Time: 0 ms (100%), Space: 13.5 MB (73.44%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums):
+        element=None
+        count=0
+        for num in nums:
+            if count==0:
+                element=num
+            if num==element:
+                count+=1
+            else:
+                count-=1
+        return element
+            
